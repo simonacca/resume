cc158614f2bf3b85b9c6c82c04cec295b0b1ca89
Simon Accascina <simon@accascina.me>
2018-02-26 18:19:41 +0100

    Initial commit, empty resume

---
+++ src/Interests.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import { Header } from 'semantic-ui-react'
+
+
+const C:React.SFC = p => (
+  <Header>My bio</Header>
+)
+
+export default C;
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import Document from './Document'
+
+ReactDOM.render(
+  <Document />,
+  document.getElementById('root') as HTMLElement
+);
